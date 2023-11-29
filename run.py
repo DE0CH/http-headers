0ed04f0e6b94458a6c13ad769bb3cc59c5586bbd
Deyao Chen <chendeyao000@gmail.com>
2023-11-29 20:53:46 +0000

    init commit

---
+++ run.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class EchoHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        # Write the headers sent by the client to the response
+        for header, value in self.headers.items():
+            response = f"{header}: {value}\n"
+            self.wfile.write(response.encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=EchoHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting httpd on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
