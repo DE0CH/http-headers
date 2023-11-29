91ba8a3a01999184fc381f9a420a7d4ea6f58c0d
Deyao Chen <chendeyao000@gmail.com>
2023-11-29 21:05:00 +0000

    change port

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ class EchoHandler(BaseHTTPRequestHandler):
             response = f"{header}: {value}\n"
             self.wfile.write(response.encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=EchoHandler, port=8080):
+def run(server_class=HTTPServer, handler_class=EchoHandler, port=8081):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting httpd on port {port}...")
